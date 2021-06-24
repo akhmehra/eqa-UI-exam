ef86770f9b15dd2768d927f7a569e58d85eb752a
Akhilesh <akhilesh.mehra@gmail.com>
2021-06-25 03:15:49 +0530

    Exam for Ascendum for WebDriverIO

--- pages/gridPage.ts
+++ pages/gridPage.ts
@@ -10,9 +10,7 @@ export class GridPage {
   }
 
   public static get gridContainer() {
-    return $(
-      '[ref="eRootWrapper"] > .ag-root-wrapper-body [ref="eCenterContainer"]'
-    );
+    return $('[ref="eRootWrapper"] > .ag-root-wrapper-body [ref="eCenterContainer"]' );
   }
 
   public static get colName() {
@@ -25,5 +23,45 @@ export class GridPage {
 
   public static get columnHeader() {
     return $$('[aria-rowindex="1"] > [role="columnheader"]');
+  }
+
+  public static get primaryHeaders() {
+    return $$('//span[@ref="agLabel"][1]');
+  }
+
+  public static get columnSidebar() {
+    return $('//span[contains (text(),"Columns")]');
+  }
+
+  public static get columnSidebarOption() {
+    return $$('//div[contains(@class,"ag-column-select-column-group")]/span[@ref="eLabel"]');
+  }
+
+  public static get secondaryColumnOption() {
+    return $$("//span[@ref='eText'][1]");
+  }
+
+  public static get sideBarColumnOption() {
+    return $$('//div[@class="ag-column-select-column ag-column-select-add-group-indent ag-column-select-indent-1"]/span[@class="ag-column-select-column-label"]');
+  }
+
+  public static get sideBarColumn() {
+    return $('//span[contains(text(),"Columns")]');
+  }
+
+  public static get nameFilterInput() {
+    return $('//input[@class="ag-floating-filter-input"]');
+  }
+
+  public static get nameCellValue() {
+    return $$('//span[@class="ag-cell-value"]');
+  }
+
+  public static get columnDataOct() {
+    return $$('//div[contains(@class,"currencyCell") and contains(@col-id,"oct")]');
+  }
+
+  public static get scrollHorizontalElement() {
+    return $('//div[contains(@class,"ag-body-horizontal-scroll-viewport")]');
   }
 }
--- tests/grid.test.ts
+++ tests/grid.test.ts
@@ -2,6 +2,11 @@ import { expect } from "chai";
 import { GridPage } from "../pages/gridPage";
 import { Page } from "../pages/page";
 
+const headerText=[];
+const secondaryColumnOptions=[];
+const sideBarColumnOptions= [];
+const namesToFilter = ["Gil", "Tony", "Isabella", "Poppy"];
+
 describe("Load AG Grid", () => {
   before(() => {
     Page.open();
@@ -19,4 +24,99 @@ describe("Load AG Grid", () => {
       expect(el.isDisplayed()).to.eql(true);
     });
   });
+
+  it("Validate Primary Headers", () => {
+    //Clicking the sidebar Column to hide
+    GridPage.sideBarColumn.click();
+    browser.pause(2000);
+
+    //Used For Loop to get all the Primary Header and save it to an array for which text is not empty
+    for(let i=0;i<GridPage.primaryHeaders.length;i++){
+      if(GridPage.primaryHeaders[i].getText()!=null || GridPage.primaryHeaders[i].getText()!=""){
+        headerText.push(GridPage.primaryHeaders[i].getText());
+      }
+    }
+
+    //Validating whether fetched data in array consist expected data
+    expect(headerText.includes("Game of Choice")).to.be.true;
+    expect(headerText.includes("Participant")).to.be.true;
+    expect(headerText.includes("Performance")).to.be.true;
+  });
+
+  it("Validate Primary Headers are available in the Columns Sidebar options", () => {
+    //Clicking the sidebar Column to hide
+    GridPage.sideBarColumn.click();
+    browser.pause(2000);
+
+    /*
+    * Used For Loop to get all the Primary Header of sidebar column and save it to an array and 
+    * validate the same with headerText Saved in Previous it block 
+    **/
+    for(let i=0;i<GridPage.columnSidebarOption.length;i++){
+      expect(headerText.includes(GridPage.columnSidebarOption[i].getText())).to.be.true;
+    }
+
+    //Clicking the sidebar Column to unhide
+    GridPage.sideBarColumn.click();
+    browser.pause(2000);
+  });
+
+  it("Validate Secondary Headers are available in the Columns Sidebar options", () => {
+    //Clicked Side Bar to hide
+    GridPage.sideBarColumn.click();
+    browser.pause(2000);
+
+    //Used For Loop to get all the Secondary Header of sidebar column and save it to an array 
+    for(let i=0;i<GridPage.secondaryColumnOption.length;i++){
+      if(GridPage.secondaryColumnOption[i].getText()!=null || GridPage.secondaryColumnOption[i].getText()!=""){
+        secondaryColumnOptions.push(GridPage.secondaryColumnOption[i].getText());
+      }
+    }
+
+    //Clicked Side Bar to unhide
+    GridPage.sideBarColumn.click();
+    browser.pause(2000);
+
+    //Used For Loop to get all the Secondary Header in sidebar column option and save it to an array
+    for(let j=0;j<GridPage.sideBarColumnOption.length;j++){
+      if(GridPage.sideBarColumnOption[j].getText()!=null || GridPage.sideBarColumnOption[j].getText()!=""){
+        sideBarColumnOptions.push(GridPage.sideBarColumnOption[j].getText());
+      }
+    }
+    
+  });
+
+  it("Validate Filtering Name will return result", () => {
+    // For Loop used to set value as per the input name array and validate the same hence two loop used
+    for(let i=0;i<namesToFilter.length;i++){
+      GridPage.nameFilterInput.setValue(namesToFilter[i]);
+      browser.pause(2000);
+
+      for(let j=0;j<GridPage.nameCellValue.length;j++){
+        expect(GridPage.nameCellValue[j].getText().includes(namesToFilter[i])).to.be.true;
+      }
+    }
+
+    GridPage.nameFilterInput.click();
+    browser.pause(2000);
+
+    //For Loop user to Clear Field
+    for(let k=0;k<5;k++){
+      browser.keys("Backspace");
+    }
+  });
+
+  it("Validate Winnings in October is less than 5000", () => {
+    GridPage.scrollHorizontalElement.click();
+    browser.pause(8000);
+    
+    //For Loop used to validate winning value and validate the same
+    for(let i=0;i<GridPage.columnDataOct.length;i++){
+      var text=GridPage.columnDataOct[i].getText().replace("$", "");
+      text= GridPage.columnDataOct[i].getText().replace(",", "");
+      
+      expect(parseInt(GridPage.columnDataOct[i].getText())).to.be.below(5000);
+    }
+  });
+
 });
